e9fa3a0df84bc24ab72d76db2cd23db0784bf390
eduardo.reoli1983@gmail.com <eduardo.reoli1983@gmail.com>
2023-05-05 00:29:38 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+//página mensagem.html
 var urlParams = new URLSearchParams(window.location.search);
 var nome = urlParams.get('nome');
 document.querySelector("#nomeDoCliente").innerHTML = nome;
 
-
+//calculo dos valores
 function calcular(){
     const choco = document.querySelector("#donutsDeChocolate").value;
     const morango = document.querySelector("#donutsDeMorango").value;
@@ -13,13 +14,23 @@ function calcular(){
     const taxaMinuto = minutosParaRetirarDonuts * 0.01;
     const total = subtotal + taxaMinuto;
 
-    
+    if (minutosParaRetirarDonuts===""){
+        alert ("Informe o tempo de retirada!");
+        return;
+    }
+
+
     document.querySelector("#subtotal").value = subtotal;
     document.querySelector("#taxas").value = taxaMinuto;
     document.querySelector("#total").value = total;
 }
 
+//envia os dados
 function enviar(){
     var nome = document.querySelector("#nome").value;
+    if(nome === ""){
+        alert("Por favor, insira seu nome.");
+        return;
+    }
     window.location.href = "mensagem.html?nome=" + nome;
 }
